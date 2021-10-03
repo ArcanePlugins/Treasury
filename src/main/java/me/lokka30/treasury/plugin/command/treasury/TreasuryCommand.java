8efb48c9b3793bdccd57c34c2cc4639e0adcb234
lokka30 <lokka30@protonmail.com>
2021-10-03 08:04:59 +0800

    Changes: * Now using the MIT License.
    Took 3 minutes

--- src/main/java/me/lokka30/treasury/plugin/command/treasury/TreasuryCommand.java
+++ src/main/java/me/lokka30/treasury/plugin/command/treasury/TreasuryCommand.java
@@ -1,22 +1,13 @@
 /*
- * Copyright (c) 2021 lokka30.
- * This code is part of Treasury, an Economy API for Minecraft servers.
+ * Copyright (c) 2021-2021 lokka30.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This code is part of Treasury, an Economy API for Minecraft servers. Please see <https://github.com/lokka30/Treasury> for more information on this resource.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * You have received a copy of the GNU Affero General Public License
- * with this program - please see the LICENSE.md file. Alternatively,
- * please visit the <https://www.gnu.org/licenses/> website.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  *
- * Please see <https://github.com/lokka30/Treasury> for more information on this resource.
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package me.lokka30.treasury.plugin.command.treasury;
