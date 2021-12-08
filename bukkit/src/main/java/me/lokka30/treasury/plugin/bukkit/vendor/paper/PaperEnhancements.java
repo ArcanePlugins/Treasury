5db24d4ba2a95d0302ab4e468047359ae8b5e98c
lokka30 <lokka30@protonmail.com>
2021-12-08 18:37:06 +0800

    update copyright, edit README.md, add CONTRIBUTING.md, edits to Wiki pages (not part of commit)

--- bukkit/src/main/java/me/lokka30/treasury/plugin/bukkit/vendor/paper/PaperEnhancements.java
+++ bukkit/src/main/java/me/lokka30/treasury/plugin/bukkit/vendor/paper/PaperEnhancements.java
@@ -1,3 +1,11 @@
+/*
+ * This file is/was part of Treasury. To read more information about Treasury such as its licensing, see <https://github.com/lokka30/Treasury>.
+ */
+
+/*
+ * This file is/was part of Treasury. To read more information about Treasury such as its licensing, see <https://github.com/lokka30/Treasury>.
+ */
+
 package me.lokka30.treasury.plugin.bukkit.vendor.paper;
 
 import java.util.Arrays;
