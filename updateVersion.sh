003b4a085d8b45e7ccc6769cf3eeba423a8e0a64
Ivan Pekov <ivan@mrivanplays.com>
2022-01-12 19:04:32 +0200

    change: version format
    Related to #105
    
    Changes version inside "plugin.yml" to "major.minor.patch-git.commit.id.abbrev-RELEASE/SNAPSHOT".
    Changes jar name to "treasury-${artifactId}-${major.minor.patch}-${git.commit.id.abbrev}-RELEASE/SNAPSHOT"
    
    Adds a new updateVersion.sh script, which makes updating the version of the plugin much easier.

---
+++ updateVersion.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# ================================================================================================
+# A shell script to update the maven version
+# This updates the "version" tag in the parent pom, and updates the
+# plugin.version and plugin.specificationVersion properties , also cleaning up the
+# "pom.xml.versionsBackup" files leftover the maven commands
+# WARNING: THIS DOES NOT UPDATE THE VERSIONS OF THE API MODULES. YOU HAVE TO DO THAT BY HAND IF
+# NEEDED.
+# ================================================================================================
+# How to use: if you just want to change the version, plainly run "./updateVersion.sh" in the 
+# terminal and it will tell you to specify a new version.
+# If you want to change the version and commit, you have to run "./updateVersion.sh commit" in 
+# the terminal and it will again tell you to specify a new version.
+# If you want to change the version, commit and push, you have to run "./updateVersion.sh commit 
+# push" in the terminal and it will again tell you to specify a new version.
+# ================================================================================================
+
+function commit {
+    version=$1
+    push=$2
+    git add "pom.xml"
+    for f in * ; do
+        if [ -d "$f" ] && [ -f "$f"/pom.xml ]; then
+            git add "$f"/pom.xml
+        fi
+    done
+    git commit -m "(updateVersion.sh) Bump version to ${version}"
+    if [ -n "$push" ] && [ "$push" = "push" ]; then
+        git push
+    fi
+}
+
+read -p "Enter new version:" input
+IFS='-'
+
+read -a array <<<"$input"
+arrayLen="${#array[@]}"
+
+# Sets the maven versions
+mvn versions:set -DnewVersion="${input}"
+mvn versions:commit
+
+# Updates the properties
+if [ "$arrayLen" -eq 1 ]; then
+    mvn versions:set-property -Dproperty=plugin.version -DnewVersion="${array[0]}"
+    mvn versions:set-property -Dproperty=plugin.specificationVersion -DnewVersion="RELEASE"
+else
+    mvn versions:set-property -Dproperty=plugin.version -DnewVersion="${array[0]}"
+    mvn versions:set-property -Dproperty=plugin.specificationVersion -DnewVersion="${array[1]}"
+fi
+
+# Remove the pom.xml.versionsBackup file of the parent pom, because we don't need it
+rm -rf pom.xml.versionsBackup
+
+echo "Version changed successfully to ${input}"
+
+if [ -n "$1" ] && [ "$1" = "commit" ]; then
+    commit "$input" "$2"
+fi
