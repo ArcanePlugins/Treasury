54c21d094cde674bb5eb5825751a74694e4976d3
lokka30 <lokka30@protonmail.com>
2022-01-23 21:46:51 +0800

    Update description

--- core/src/main/java/me/lokka30/treasury/plugin/core/TreasuryPlugin.java
+++ core/src/main/java/me/lokka30/treasury/plugin/core/TreasuryPlugin.java
@@ -27,8 +27,10 @@ public abstract class TreasuryPlugin {
     /**
      * Description of the plugin.
      */
-    public static final String DESCRIPTION = "Treasury is a modern, multi-platform library facilitating the interaction between" +
-            " 'provider' plugins on your server.";
+    public static final String DESCRIPTION =
+            "Treasury is a modern multi-platform library facilitating " +
+                    "the integration between service providing/consuming " +
+                    "plugins on Minecraft servers. ";
 
     private static TreasuryPlugin instance;
 
