b89e9c895a4e32f88b58104b1b7f5fa1946862aa
Ivan Pekov <ivan@mrivanplays.com>
2022-02-10 09:03:52 +0200

    Did I ever say bukkit is stupid? Well now I do.

--- platform/bukkit/api/src/main/java/me/lokka30/treasury/api/economy/event/NonPlayerAccountTransactionEvent.java
+++ platform/bukkit/api/src/main/java/me/lokka30/treasury/api/economy/event/NonPlayerAccountTransactionEvent.java
@@ -35,17 +35,4 @@ public class NonPlayerAccountTransactionEvent extends AccountTransactionEvent {
         return (NonPlayerAccount) super.getAccount();
     }
 
-    public static HandlerList HANDLERS = new HandlerList();
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLERS;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLERS;
-    }
-
 }
