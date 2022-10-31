36398747a8527bf0972ded442ad0e49d5f9f770f
Ivan Pekov <ivan@mrivanplays.com>
2022-10-31 17:47:49 +0200

    headers

--- api/src/main/java/me/lokka30/treasury/api/economy/response/EconomyFailureReason.java
+++ api/src/main/java/me/lokka30/treasury/api/economy/response/EconomyFailureReason.java
@@ -1,3 +1,7 @@
+/*
+ * This file is/was part of Treasury. To read more information about Treasury such as its licensing, see <https://github.com/lokka30/Treasury>.
+ */
+
 package me.lokka30.treasury.api.economy.response;
 
 import java.math.BigDecimal;
