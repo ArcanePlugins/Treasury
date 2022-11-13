301df78ceba98138f244a6ad3370cafb3ac5f19a
Ivan Pekov <ivan@mrivanplays.com>
2022-11-13 18:42:58 +0200

    file headers

--- core/src/main/java/me/lokka30/treasury/plugin/core/command/subcommand/economy/migrate/CurrenciesMigrator.java
+++ core/src/main/java/me/lokka30/treasury/plugin/core/command/subcommand/economy/migrate/CurrenciesMigrator.java
@@ -1,3 +1,7 @@
+/*
+ * This file is/was part of Treasury. To read more information about Treasury such as its licensing, see <https://github.com/ArcanePlugins/Treasury>.
+ */
+
 package me.lokka30.treasury.plugin.core.command.subcommand.economy.migrate;
 
 import java.math.BigDecimal;
