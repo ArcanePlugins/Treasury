06b8cc22ece3b7404be8011f4482326208421bcc
Ivan Pekov <ivan@mrivanplays.com>
2022-11-29 17:29:57 +0200

    NodeHolderGroup

---
+++ api/src/main/java/me/lokka30/treasury/api/permission/node/holder/NodeHolderGroup.java
@@ -0,0 +1,9 @@
+/*
+ * This file is/was part of Treasury. To read more information about Treasury such as its licensing, see <https://github.com/ArcanePlugins/Treasury>.
+ */
+
+package me.lokka30.treasury.api.permission.node.holder;
+
+public interface NodeHolderGroup extends NodeHolder {
+
+}
