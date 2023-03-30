98b3983143fab9b23a233743fe90e617b32c2966
Ivan Pekov <ivan@mrivanplays.com>
2023-03-30 08:18:00 +0300

    Update api/src/main/java/me/lokka30/treasury/api/economy/EconomyProvider.java
    Co-authored-by: Lachlan <59464084+lokka30@users.noreply.github.com>

--- api/src/main/java/me/lokka30/treasury/api/economy/EconomyProvider.java
+++ api/src/main/java/me/lokka30/treasury/api/economy/EconomyProvider.java
@@ -195,7 +195,7 @@ public interface EconomyProvider {
      *
      * @param currency The currency to un-register with the {@link EconomyProvider}.
      * @return a {@link TriState} value representing whether the unregistration was successful. If
-     *         the currency was successfully un-registered, this shall be {@link TriState#TRUE},
+     *         the currency was successfully unregistered, {@link TriState#TRUE} is returned,
      *         otherwise {@link TriState#FALSE} and if that currency is not registered already,
      *         {@link TriState#UNSPECIFIED}.
      * @since v2.0.0
