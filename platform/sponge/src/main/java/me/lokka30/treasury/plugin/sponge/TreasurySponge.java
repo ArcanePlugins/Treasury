0ec28fee17bf96950586eac15be23feced10d6d3
Ivan Pekov <ivan@mrivanplays.com>
2023-04-19 21:33:43 +0300

    Temporarily disable metrics in sponge
    Something's wrong with the metrics class

--- platform/sponge/src/main/java/me/lokka30/treasury/plugin/sponge/TreasurySponge.java
+++ platform/sponge/src/main/java/me/lokka30/treasury/plugin/sponge/TreasurySponge.java
@@ -75,7 +75,7 @@ public class TreasurySponge {
         TreasuryPlugin.setInstance(treasuryPlugin);
 
         UpdateChecker.checkForUpdates();
-        loadMetrics();
+        //loadMetrics(); // TODO: Something's wrong with the metrics class. Needs investigation
 
         treasuryPlugin.logStartupMessage(startupTimer, true);
     }
